a3b698b1ca0883673d91ce4549c0b56c23677664
Johann Cortez <jn20x@hotmail.com>
2023-11-26 22:51:50 -0500

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy( value: number): void {
+    this.counter += value;
+  }
+  resetCounter() {
+    this.counter = 10;
+  }
 }
