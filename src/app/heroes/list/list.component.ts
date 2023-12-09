13538aee2d4a07fe924bdd7643ee5145427d5335
Johann Cortez <jn20x@hotmail.com>
2023-12-09 13:48:30 -0500

    Fin de sección 4

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string;
+
+  removeLastHero():void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
