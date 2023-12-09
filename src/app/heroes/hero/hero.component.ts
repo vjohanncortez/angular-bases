3dcfae2e222d53162de2b29d7c8b2be8a1957540
Johann Cortez <jn20x@hotmail.com>
2023-12-08 23:35:30 -0500

    ng if

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-heroes-hero',
@@ -9,6 +10,31 @@ export class HeroComponent {
 
   public name: string = 'ironman';
   public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
+  changeHero():void {
+    this.name = 'spiderman';
+  }
+
+  changeAge():void {
+    this.age = 25;
+  }
+
+  resetForm():void {
+    this.name = 'Ironman';
+    this.age = 45;
+
+    // document.querySelectorAll('h1')!.forEach( e => {
+    //   e.innerHTML = '<h1>Desde Angular</h1>';
+    // })
+  }
 
 
 }
